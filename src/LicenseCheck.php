56c15476a7fef5b613eacc40feff84780a0444a4
gary <chaisoo3333@gmail.com>
2021-03-09 13:09:09 +0800

    test

--- src/LicenseCheck.php
+++ src/LicenseCheck.php
@@ -27,10 +27,6 @@ trait LicenseCheck
         if (config('app.debug') == 'true' && config('app.env') == 'local') {
             return;
         }
-
-        if (! $this->validCode(config('backpack.base.license_code'))) {
-            \Alert::add('warning', "<strong>You're using unlicensed software.</strong> Please ask your web developer to <a target='_blank' class='alert-link text-white' href='http://backpackforlaravel.com'>purchase a license code</a> to hide this message.");
-        }
     }
 
     /**
